5b5870c9c9e78ef59b3153d05203199b4cf89f59
İsmail Çolak <ghismailcolakk13@gmail.com>
2026-06-24 11:49:54 +0300

    .

--- Backend/DigitalLoanSystem.Core/obj/Debug/net10.0/DigitalLoanSystem.Core.AssemblyInfo.cs
+++ Backend/DigitalLoanSystem.Core/obj/Debug/net10.0/DigitalLoanSystem.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalLoanSystem.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89defb4a4adbdd8801dfc6cd5eb37d5dcda4cf9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a52f6be0454d59f428f47d1c39efde260f17607d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalLoanSystem.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalLoanSystem.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
